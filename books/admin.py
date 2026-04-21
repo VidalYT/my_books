85852718cfa88f8a50051cc89038a9a55cdcb9a7
VidalYT <vidalgames12345@gmail.com>
2026-04-20 19:30:10 -0700

    We added some lines of codes in the admin's py and models's py

---
+++ books/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+
+# Adding some lines for the Admin funcions.
+from .models import Category, Book
+
+admin.site.register(Category)
+admin.site.register(Book)
---
+++ books/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# New seccion of the code added (To get All URL Generated).
+class Category(models.Model):
+    title = models.CharField(max_length=250) 
+
+    class Meta:
+        ordering = ('title',)
+        verbose_name_plural = 'Categories'
+    
+    def __str__(self):
+        return self.title
+    
+
+class Book(models.Model):
+
+    title = models.CharField(max_length=250)  
+    author = models.CharField(max_length=250)  
+    image = models.CharField(max_length=2000) 
+    created_at =models.CharField(auto_now_add=True) 
+    update_at = models.CharField(auto_now=True) 
+
